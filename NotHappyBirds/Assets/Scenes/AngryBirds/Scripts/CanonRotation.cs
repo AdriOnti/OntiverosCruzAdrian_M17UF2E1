deb9099a6821d026ef51ec1c3fd4373320c5b0ae
AdriOnti <adrian.ontiveros.7e4@itb.cat>
2024-01-26 17:42:41 +0100

    Entrega

--- NotHappyBirds/Assets/Scenes/AngryBirds/Scripts/CanonRotation.cs
+++ NotHappyBirds/Assets/Scenes/AngryBirds/Scripts/CanonRotation.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class CanonRotation : MonoBehaviour
@@ -9,7 +10,7 @@ public class CanonRotation : MonoBehaviour
     private float offset = -51.6f;
     public GameObject ShootPoint;
     public GameObject Bullet;
-    public float ProjectileSpeed = 0;
+    public float ProjectileSpeed;
     public float MaxSpeed;
     public float MinSpeed;
     public GameObject PotencyBar;
@@ -19,30 +20,33 @@ public class CanonRotation : MonoBehaviour
     {
         initialScaleX = PotencyBar.transform.localScale.x;
     }
+
     void Update()
     {
-        var mousePos = //guardem posició de la càmera
-        var dist = //distància entre el click i la bala
-        var ang = (Mathf.Atan2(dist.y, dist.x) * 180f / Mathf.PI + offset);
-        transform.rotation = Quaternion.Euler(//angle que s'ha de rotar
 
-        if(Input.GetMouseButton(0))
+        var mousePos = Input.mousePosition;
+        var dist = mousePos - transform.position;
+        var ang = (Mathf.Atan2(dist.y, dist.x) * 360 / Mathf.PI + offset);
+        transform.rotation = Quaternion.Euler(0f, 0f, ang);
+
+        if (Input.GetMouseButton(0))
         {
-            ProjectileSpeed += //cada frame s'ha de fer 4 cops més gran
+            ProjectileSpeed += ProjectileSpeed * 4 * Time.deltaTime;
+            CalculateBarScale(Mathf.Lerp(initialScaleX, 1, ProjectileSpeed / MaxSpeed));
         }
-        if(Input.GetMouseButtonUp(0))
+
+        if (Input.GetMouseButtonUp(0))
         {
-            var projectile = Instantiate(Bullet, //On s'instancia?
-            projectile.GetComponent<Rigidbody2D>().velocity = //quina velocitat ha de tenir la bala? s'ha de fer alguna cosa al vector direcció?
-            ProjectileSpeed = 0f;
+            var projectile = Instantiate(Bullet, ShootPoint.transform.position, transform.rotation);
+            projectile.GetComponent<Rigidbody2D>().velocity = ShootPoint.transform.up * ProjectileSpeed;
+            ProjectileSpeed = 1f;
+            CalculateBarScale(0f);
         }
-        CalculateBarScale();
 
     }
-    public void CalculateBarScale()
+
+    public void CalculateBarScale(float x)
     {
-        PotencyBar.transform.localScale = new Vector3(Mathf.Lerp(0, initialScaleX, ProjectileSpeed / MaxSpeed),
-            transform.localScale.y,
-            transform.localScale.z);
+        PotencyBar.transform.localScale = new Vector3(x, transform.localScale.y, transform.localScale.z);
     }
 }
